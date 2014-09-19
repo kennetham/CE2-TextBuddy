4bb982c1208736f1c5fe1ac8a9f6ce9e41d52ab9
Kenneth Ham <kenneth.ham@comp.nus.edu.sg>
2014-09-19 23:45:12 +0800

    Version 0.1.1: Added jUnit Test for TextBuddy++ [Test Case Fail: Not implemented]

---
+++ src/test/TextBuddyTest.java
@@ -0,0 +1,50 @@
+/*
+ * National University of Singapore
+ * The MIT License (MIT)
+ *
+ * Copyright (c) 2014. Kenneth Ham Gao Jie (A0111875E)
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package test;
+
+import com.CS2103.TextBuddy_v2.*;
+import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.*;
+
+import java.io.IOException;
+import java.util.List;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * Created by kennetham on 18/09/14.
+ */
+public class TextBuddyTest {
+	private static final String APP_NAME = "TextBuddy";
+	private static final String FILE_NAME = "in.txt";
+	private static final String[] args = {APP_NAME, FILE_NAME};
+
+	@Test
+	public void testFailCase() {
+		fail("Not Implemented");
+	}
+}
